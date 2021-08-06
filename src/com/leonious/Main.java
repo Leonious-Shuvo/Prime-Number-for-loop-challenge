9eb580cbff7d440e16e807a49b9b80345fee1dfb
Shubrato Dev Baishnab <shuvodev1994@gmail.com>
2021-08-07 01:47:51 +0600

    for loop prime number challenge java

---
+++ src/com/leonious/Main.java
@@ -0,0 +1,27 @@
+package com.leonious;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //int count =0;
+        for( int i=10; i<50; i++) {
+            if(isPrime(i)) {
+                //count--;
+                System.out.println("Number " + i + " is Prime Number");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if(n==1){
+            return false;
+        }
+        for(int i=2; i <= n/2; i++ ) {
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
